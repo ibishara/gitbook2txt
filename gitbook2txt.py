af8b0bdcd3f8c56addb8b61b5589193a4b1702c6
ibishara <87097444+ibishara@users.noreply.github.com>
2023-11-27 20:59:49 -0800

    Create gitbook2txt.py

---
+++ gitbook2txt.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+# Function to download a single page
+def download_page(url, folder):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Extract text content
+        text = soup.get_text()
+
+        # Format the filename
+        title = soup.title.string.split('|')[0].strip().replace(' ', '_').replace('/', '_')
+        filename = f"{folder}/{title}.txt"
+
+        # Save the file in text format
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(text)
+            print(f"Downloaded: {filename}")
+
+    except Exception as e:
+        print(f"Error downloading page {url}: {e}")
+
+# Main function to download the entire Gitbook
+def download_gitbook(main_url):
+    try:
+        response = requests.get(main_url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Create a directory for the GitBook
+        book_title = soup.title.string.split('|')[0].strip().replace(' ', '_').replace('/', '_')
+        folder = f"GitBook_{book_title}"
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        # Find all links in the GitBook
+        links = soup.find_all('a')
+        page_urls = set()
+
+        for link in links:
+            href = link.get('href')
+            if href and not href.startswith('http'):
+                full_url = requests.compat.urljoin(main_url, href)
+                page_urls.add(full_url)
+
+        # Download each page
+        for url in page_urls:
+            download_page(url, folder)
+
+    except Exception as e:
+        print(f"Error downloading GitBook: {e}")
+
+# Example usage
+gitbook_url = "https://docs.gitbook.com/"  # Replace with the actual GitBook URL
+download_gitbook(gitbook_url)
